49ea3117f75ea23fd6b53127f2557a345e0fd26d
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-09 09:03:26 -0800

    Add substring search functionality

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,13 @@
+def substrings(string, dictionary)
+  result = Hash.new(0)
+  lowercase_str = string.downcase
+  dictionary.each do |word|
+    matches = lowercase_str.scan(word).length
+    result[word] = matches
+  end
+  puts result
+end
 
+dict = %w(el lo he zr ol)
+
+substrings("hello", dict)
